73a65339ffdf6ea13f38339c0203131443afa557
George Hamann <george.hamann@gmail.com>
2024-02-13 21:48:15 -0300

    Autenticação com criação de usuario simples

--- ProjetoEmTresCamadas.Pizzaria.Mvc/Models/Login.cs
+++ ProjetoEmTresCamadas.Pizzaria.Mvc/Models/Login.cs
@@ -11,4 +11,23 @@ namespace ProjetoEmTresCamadas.Pizzaria.Mvc.Models
         [Required]        
         public string password { get; set; }
     }
+
+    public class ClienteCreate
+    {
+        [Required]
+        [EmailAddress(ErrorMessage = "Entre com um email válido")]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        [Required]
+        public string Nome { get; set; }
+
+
+        [Required]
+        public string ConfirmPassword { get; set; }
+
+    }
+
 }
